ec6723b5233f4962549cc27681e27bf50f6ba15d
Alex <alex@matica.com>
2017-01-10 18:37:11 +0000

    more bigtrack like

--- robot.py
+++ robot.py
@@ -19,19 +19,21 @@ def stop():
     motor_b_direction_control.ChangeDutyCycle(0)
     motor_b_speed_control.ChangeDutyCycle(0)
 
-def forward(speed):
-    stop()
+def forward(speed,duration=1):
+    # stop()
     motor_a_direction_control.ChangeDutyCycle(0)
     motor_a_speed_control.ChangeDutyCycle(speed)
     motor_b_direction_control.ChangeDutyCycle(0)
     motor_b_speed_control.ChangeDutyCycle(speed)
+    time.sleep(duration)
 
-def reverse(speed):
-    stop()
+def reverse(speed,duration=1):
+    # stop()
     motor_a_direction_control.ChangeDutyCycle(speed)
     motor_a_speed_control.ChangeDutyCycle(0)
     motor_b_direction_control.ChangeDutyCycle(speed)
     motor_b_speed_control.ChangeDutyCycle(0)
+    time.sleep(duration)
 
 motor_b_speed_control=GPIO.PWM(motor_b_direction_pin,50)
 motor_b_direction_control=GPIO.PWM(motor_b_speed_pin,50)
@@ -42,14 +44,11 @@ motor_a_speed_control=GPIO.PWM(motor_a_direction_pin,50)
 motor_a_direction_control=GPIO.PWM(motor_a_speed_pin,50)
 motor_a_speed_control.start(0)
 motor_a_direction_control.start(0)
-forward(10)
-time.sleep(5)
-forward(50)
-time.sleep(5)
-reverse(10)
-time.sleep(5)
-reverse(50)
-time.sleep(5)
-reverse(25)
-time.sleep(5)
+
+forward(15,2)
+forward(50,1)
+reverse(15,2)
+reverse(50,1)
+reverse(25,1)
+forward(25,1)
 stop()
